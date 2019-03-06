f431fee60cbf1f75202778bef4ccb53691aba857
unknown <mport.dev@gmail.com>
2019-03-06 07:39:04 -0300

    Created app start files

---
+++ src/app/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import buildRoutes from '../infrastructure/routers';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.start = () => new Promise(async (resolve) => {
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  buildRoutes(app);
+  resolve();
+});
+
+app.port = port;
+
+export default app;
