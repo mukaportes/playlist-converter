dc7a1274e132cde8711dfb66d2800cf2a738b145
unknown <mport.dev@gmail.com>
2019-03-10 22:23:44 -0300

    Updated Spotify variables

--- src/config/options/local.js
+++ src/config/options/local.js
@@ -2,6 +2,12 @@ export default {
   logger: {
     level: process.env.LOGGER_LEVEL,
   },
+  spotify: {
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    url: process.env.SPOTIFY_URL,
+    authUrl: process.env.AUTH_SPOTIFY_URL,
+  },
   youtube: {
     apiKey: process.env.YOUTUBE_APIKEY,
     url: process.env.YOUTUBE_URL,
